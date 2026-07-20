66160ec105687e1ed9fcb50120706ee0d3471b71
shivang-5432 <shivangaggarwal5432@gmail.com>
2026-07-21 02:17:56 +0530

    Rendering tasks from a local hard-coded array

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,16 +1,41 @@
+import type { Task } from "../types/Task";
 import TaskCard from "./TaskCard";
 
 export default function Board() {
+
+    const toDoTasks: Task[] = [{title: "toDo1", description: "desc1"}, {title: "toDo2", description: "desc2"}];
+    const inProgressTasks: Task[] = [];
+    const doneTasks: Task[] = [{title: "done1", description: "desc1"}];
+
     return (
         <div className="grid flex-1 grid-cols-3">
-            <div className="border border-gray-900 rounded-bl-lg">
-                <TaskCard
-                    title="Create ToDo List"
-                    description="Create ToDo List App using React with TailwindCSS"
-                 />
+            <div className="border border-gray-900 rounded-bl-lg flex flex-col gap-4 p-4">
+                {toDoTasks.map((task) => (
+                    <TaskCard
+                        key={task.title}
+                        title={task.title}
+                        description={task.description}
+                    />
+                ))}
             </div>
-            <div className="border border-gray-900"></div>
-            <div className="border border-gray-900 rounded-br-lg"></div>
+            <div className="border border-gray-900 flex flex-col gap-4 p-4">
+                {inProgressTasks.map((task) => (
+                    <TaskCard
+                        key={task.title}
+                        title={task.title}
+                        description={task.description}
+                    />
+                ))}
+            </div>
+            <div className="border border-gray-900 rounded-br-lg flex flex-col gap-4 p-4">
+                {doneTasks.map((task) => (
+                    <TaskCard
+                        key={task.title}
+                        title={task.title}
+                        description={task.description}
+                    />    
+                ))}
+            </div>
         </div>
     )
 }
--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,9 +1,6 @@
-type TaskCardProps = {
-    title: string,
-    description: string
-}
+import type { Task } from "../types/Task";
 
-export default function TaskCard({ title, description }: TaskCardProps) {
+export default function TaskCard({ title, description }: Task) {
     return (
         <div className="rounded-xl bg-white p-4 shadow-md">
             <h3 className="text-lg font-semibold text-gray-800">
